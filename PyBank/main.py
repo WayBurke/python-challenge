8e53a4b3687ca07b8862abb593e5a008f0d439ff
WayBurke <104881082+WayBurke@users.noreply.github.com>
2022-07-11 01:51:26 -0500

    Updated main.py for both PyPoll and PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,6 +10,7 @@
 #====================================================================================================
 
 
+from operator import ne
 import os
 import csv
 
@@ -19,11 +20,15 @@ csvpath = os.path.join('Resources','budget_data.csv')
 
 
 #Declaring the values with initial values
-totalMonth = 0  #row counter to determine number of rows
+totalMonth = 0  #row counter to determine number of rows, starting at 1 since first line is
 totalPL = 0
+StartValue = 0
+EndValue = 0
 averageChange =0
 profit = 0
 loss = 0
+
+csvList =[]         #CREATE A LIST TO TRACK THE ROWS OF THE TABLE
 
 
 with open(csvpath) as budgetcsvfile:
@@ -36,22 +41,55 @@ with open(csvpath) as budgetcsvfile:
         totalMonth = totalMonth + 1     #increasing the row counter
         totalPL = totalPL + int(row[1])
         
+        #ADDING A NEW ROW TO THE LIST TO TRACK THE ROWS OF THE TABLE
+        csvList.append(row)     # adding the next line of the CSV file to the List
+        
        
-        if int(row[1]) < 0:
-            loss = loss + int(row[1])
-        elif int(row[1]) >= 0:
-            profit = profit + int(row[1])
+        averageTotal = int(row[1])
+
+   
+averageChange = round((averageTotal-int(csvList[0][1]))/(totalMonth-1),2) #-->UNDER CONSTRUCTION
+
+
+#create new list
+# use this new list to store the difference
+profitMth =""
+lossMth =""
+previousValue = int(csvList[1][1]) - int(csvList[0][1])  # setting previous value to the first item in the list
+
+
+for entry in range(len(csvList)):
+    newvalue= int(csvList[entry][1]) - int(csvList[entry - 1][1])
 
     
+    #CHECK FOR GREATEST INCREASE - PROFIT
+    if (newvalue > previousValue) and (newvalue>profit):
+        profit = newvalue
+        profitMth= csvList[entry][0]
+        #loss = previousValue
+    elif (previousValue >newvalue) and (previousValue > profit):
+        profit = previousValue
+        profitMth= csvList[entry-1][0]
+        #loss = newvalue
 
-averageChange = round(totalPL/totalMonth,3)
+    
+    #CHECK FOR GREATEST DECREASE - LOSS
+    if (newvalue < previousValue) and (newvalue< loss):
+        loss = newvalue
+        lossMth = csvList[entry][0]
+    elif (previousValue <newvalue) and (previousValue <loss):
+        loss = previousValue
+        lossMth = csvList[entry -1][0]
+
+    previousValue = newvalue
+
 
 #Print out of the values calculated
 print ("_____________________")
 print("Financial Analysis")
 print ("---------------------")
 print(f"Total Months: {totalMonth}")
-print(f"Total: $ {totalPL}")
-print("Average Change: " + str(averageChange))      # CURRENT CALCULATION IS INCORRECT, should be the average of the difference btw 2
-print(f"Greatest Increase in Profits: {profit}")    # CURRENT CALCULATION IS INCORRECT, should be the highest value in the diference btw 2
-print(f"Greatest Decrease in Profits: {loss}")      # CURRENT CALCULATION IS INCORRECT, should be the lowest value in the diference btw 2
+print(f"Total: ${totalPL}")
+print(f"Average Change: $ {str(averageChange)}")  
+print(f"Greatest Increase in Profits: {profitMth} (${profit})")    
+print(f"Greatest Decrease in Profits: {lossMth} (${loss})")      
